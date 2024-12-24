01c5513184a6d12ad51c8291c5b459ab4262c9d0
Ndamulelo <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2024-12-24 05:51:15 +0200

    add client logic for login

--- client/src/app/pages/login-page/login-page.component.ts
+++ client/src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,RouterLink],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  styleUrl: './login-page.component.css',
 })
 export class LoginPageComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
 
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private router:Router
+  ) {}
+
+  onSubmit() {
+    const { email, password } = this.loginForm.value;
+    this.authService.login({ email, password }).subscribe({
+      next: (respons) => {
+        console.log(respons);
+        this.router.navigate(['/dashboard'])
+      },
+      error: (err) => {
+        console.error('Error logging in: ', err);
+      },
+    });
+  }
 }
