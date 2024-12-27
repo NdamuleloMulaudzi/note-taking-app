dc0004cdc102dee711bc9104bfcb78365d1c58d8
Ndamulelo <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2024-12-27 16:42:46 +0200

    add delete button to sidebar

--- client/src/app/components/side-bar/side-bar.component.ts
+++ client/src/app/components/side-bar/side-bar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { AddButtonComponent } from '../add-button/add-button.component';
 import { UserService } from '../../services/user.service';
 import { UserInterface } from '../../interfaces/user.interface';
+import { DeleteButtonComponent } from "../delete-button/delete-button.component";
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [AddButtonComponent],
+  imports: [AddButtonComponent, DeleteButtonComponent],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.css',
 })
